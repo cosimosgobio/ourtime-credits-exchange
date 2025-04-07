be394a9df584b8c1beba2a84d6b8cdd95c7524c5
cosimosgobio <cosimosgobio@hotmail.com>
2025-04-07 11:41:20 +0200

    Update map-view.tsx

--- src/components/ui/map-view.tsx
+++ src/components/ui/map-view.tsx
@@ -69,8 +69,8 @@ export function MapView({
         bounds={bounds}
       >
         <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url={`https://api.maptiler.com/maps/basic-v2/256/{z}/{x}/{y}.png?key=gx0LszzOzHY33jSxgeOC`}
+          attribution='&copy; <a href="https://www.maptiler.com/copyright">MapTiler</a> contributors'
         />
         {/* User location marker */}
         {userLocation && (
