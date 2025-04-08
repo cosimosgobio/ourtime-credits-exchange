6aac75cbc261e29150afab3555887b37af6d66b1
cosimosgobio <cosimosgobio@hotmail.com>
2025-04-08 10:31:10 +0200

    Update Layout.tsx

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,4 +1,3 @@
-
 import { ReactNode } from 'react';
 import { NavBar } from './NavBar';
 
@@ -8,7 +7,7 @@ interface LayoutProps {
 
 export const Layout = ({ children }: LayoutProps) => {
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="min-h-screen flex flex-col bg-gray-50">
       <NavBar />
       <main className="flex-1 pt-24 pb-16 px-4 sm:px-6">
         {children}
